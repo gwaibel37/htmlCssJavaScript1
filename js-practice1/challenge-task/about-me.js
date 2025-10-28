68e144db239f05bb0b0cd113576c14e70b79ab14
gwaibel37 <mc_gwaibel27@mcjvs2.org>
2025-10-28 08:25:02 -0400

    Added All Files

---
+++ js-practice1/challenge-task/about-me.js
@@ -0,0 +1,18 @@
+/* Gavin Waibel about Me Site Stuff
+----------------------COOL MULTILINE COMMENT!----------------------
+Send the alert */
+alert("Welcome to a website thats ALL ABOUT ME!");
+
+//Write the h3 and its contents
+document.write("<h3> GAVIN WAIBEL </h3>");
+console.log("Name Successfully displayed!");
+
+//Wrtie the three paragraphs and their contents
+document.write("<p>I am a cross country and track runner, and our current 4x800 team holds the school record.</p>");
+console.log("Paragraph Successfully Displayed!");
+
+document.write("<p>I love rock music, and I have picked up the basics of drumming in the last few months!</p>");
+console.log("Second Paragraph Successfully Displayed!");
+
+document.write("<p>I know the basics of coding in C++ and building games in godot - and </p>");
+console.log("Third Paragraph Successfully Displayed!");
---
+++ js-practice1/external/script.js
@@ -0,0 +1,5 @@
+//This is a comment with my name: Gavin Waibel!
+
+console.log("External File Loaded");
+
+document.write("<p> I Learned all about the ins-and-outs of mmaking javascript in my websites. Yu can put it in-line, in the head, or external, and it makes it dynamic and cooler.</p>")
