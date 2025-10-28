f696243247743235a38b448b6334c9101b07f7e0
gwaibel37 <mc_gwaibel27@mcjvs2.org>
2025-10-28 08:06:10 -0400

    test

---
+++ js-intro/index.js
@@ -0,0 +1,12 @@
+console.log("Hello my stuff ran brochacho!");
+
+window.alert("YOU ARE GAY");
+
+document.getElementById("myh1").textContent = "Whats the fudge brouh";
+document.getElementById("mypp").textContent = "Squidward"
+
+// This is a commment
+
+/*this is a 
+multi
+line comment*/
